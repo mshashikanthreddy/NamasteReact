dae0b9d472c9c33768997a0cfaba7d812ad2d248
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2025-10-09 12:30:02 +0530

    Using useEffect,Shimming

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,35 @@
-import resList from "../utils/mockdata";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
     // "Hooks" gives the access to use state variable and other react features without writing a class.
     //Local State Variable - super powerful variable 
-    const [ListOfRestaurants, setListOfRest]  = useState(resList); 
+    const [ListOfRestaurants, setListOfRest]  = useState([]); 
     
+    /* "React" follows Loads->render->API->re-renders.
+        which is the best practice rather than displaying blank page which is not a best practice.
 
-    /* Normal variable
-    let ListOfRestaurants = [];*/
-      return (
+    /* when user clicks on URL the body starts rendering with old data which already have and then it calls "useEffect"
+       which takes one callback function and an dependency array which re-runs when it changes.
+       "useEffect" used to fetch api's of outside sites, event listeners,handlers which re-renders the "DOM".
+    */
+   useEffect(()=> {
+    fetchData();
+   },[])
+
+   const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json();
+    console.log(json);
+
+    // Optional chaining
+    setListOfRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+   }
+
+   //conditional Rendering
+   return ListOfRestaurants.length === 0?<Shimmer />:(
           <div className="body">
               <div className="filter">
                 <button className="filter-btn"
---
+++ src/components/Shimmer.js
@@ -0,0 +1,17 @@
+// Shimming is technique which mimicks the content and forms of website by use of css which makes the better user experience
+const Shimmer = () => {
+    return <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+    </div> 
+
+}
+
+export default Shimmer;
